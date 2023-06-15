14fc7b928747c77ab74e302e16532987eeabda66
pttkachuk <petrotkachuk6@gmail.com>
2023-06-15 10:31:48 +0200

    done task 01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,21 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+};
+const buttonStart = document.querySelector('button[data-start]');
+const buttonStop = document.querySelector('button[data-stop]');
+let timerId;
 
+buttonStart.addEventListener('click', () => {
+    timerId = setInterval(() => {
+        const randomColor = getRandomHexColor();
+        document.body.style.background = randomColor;
+        buttonStart.disabled = true;
+        buttonStop.disabled = false;
+    }, 1000)
+});
+
+buttonStop.addEventListener('click', () => {
+    clearInterval(timerId);
+    buttonStop.disabled = true;
+    buttonStart.disabled = false;
+})
